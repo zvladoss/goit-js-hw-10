03784a52caa5809b86d2ca630fc909987a1ba4cb
Vladislav Zarovniy <vladislavzarovnii13@gmail.com>
2025-03-13 21:45:59 +0200

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -98,3 +98,5 @@ function convertMs(ms) {
 console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+// installed izitoast
